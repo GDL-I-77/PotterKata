cb9fec98d8b82c871eba37f1ff8e5d375b3da393
ysasim <y.sasim@godeltech.com>
2015-03-20 16:26:24 +0300

    Referenced Unity and created Bootstrapper to initialize UnityContainer.

---
+++ test/PotterKata.BusinessLogic.IntegrationTests/TestsFixture/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Unity;
+using PotterKata.BusinessLogic.Calculation;
+using PotterKata.BusinessLogic.Facades;
+using PotterKata.DataAccess.Repositories;
+using PotterKata.DataAccess.Storages;
+
+namespace PotterKata.BusinessLogic.IntegrationTests.TestsFixture
+{
+	public static class Bootstrapper
+	{
+		public static UnityContainer Initialise()
+		{
+			var container = new UnityContainer();
+			container
+				.RegisterType<IMemoryStorage, MemoryStorage>(new ContainerControlledLifetimeManager())
+				.RegisterType<IBooksRepository, BooksRepository>(new ContainerControlledLifetimeManager())
+				.RegisterType<IWishListRepository, WishListRepository>(new ContainerControlledLifetimeManager())
+				.RegisterType<ITotalPriceCalculator, TotalPriceCalculator>(new ContainerControlledLifetimeManager())
+				.RegisterType<IStoreFacade, StoreFacade>(new ContainerControlledLifetimeManager());
+
+			return container;
+		}
+	}
+}
